f961cf305bcc6f3bce50f1d64ca7ae16d3b972cd
Hugh Wang <hghwng@gmail.com>
2015-11-13 15:34:27 +0800

    Add plugin interface.

---
+++ include/plugin.h
@@ -0,0 +1,39 @@
+#ifndef PLUGIN_PLUGIN_H
+#define PLUGIN_PLUGIN_H
+#include <string>
+#include <map>
+
+struct PlgSystemAuthResponse {
+  bool result;
+  std::string message;
+};
+
+struct PlgCustomAuthResponse {
+  enum PlgCustomAuthResult {
+    kSuccess,
+    kFail,
+    kError
+  } result;
+  std::string message;
+};
+
+enum PlgAuthType {
+  kSystemAuth,
+  kCustomAuth,
+};
+
+using PlgConf = std::map<std::string, std::string>;
+using PlgSystemAuthCb = PlgSystemAuthResponse (*)(const PlgConf &sysconf, const PlgConf &plgconf);
+using PlgCustomAuthCb = PlgCustomAuthResponse (*)(const PlgConf &sysconf, const PlgConf &plgconf);
+
+struct PlgInfo {
+  std::string name;
+  PlgAuthType auth_type;
+
+  PlgSystemAuthCb sys_auth_cb;
+  PlgCustomAuthCb cust_auth_cb;
+};
+
+extern "C" bool plg_init(PlgInfo *info);
+
+#endif
